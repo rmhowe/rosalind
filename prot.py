cfaf6c74267a3283fb8352ef1ff8ef88b090ae4c
rmhowe <tiaeph@gmail.com>
2017-10-31 10:58:02 +0100

    Solution for PROT

---
+++ prot.py
@@ -0,0 +1,86 @@
+from read_file import read_file
+
+rna_codon_proteins = {
+    'UUU': 'F',
+    'CUU': 'L',
+    'AUU': 'I',
+    'GUU': 'V',
+    'UUC': 'F',
+    'CUC': 'L',
+    'AUC': 'I',
+    'GUC': 'V',
+    'UUA': 'L',
+    'CUA': 'L',
+    'AUA': 'I',
+    'GUA': 'V',
+    'UUG': 'L',
+    'CUG': 'L',
+    'AUG': 'M',
+    'GUG': 'V',
+    'UCU': 'S',
+    'CCU': 'P',
+    'ACU': 'T',
+    'GCU': 'A',
+    'UCC': 'S',
+    'CCC': 'P',
+    'ACC': 'T',
+    'GCC': 'A',
+    'UCA': 'S',
+    'CCA': 'P',
+    'ACA': 'T',
+    'GCA': 'A',
+    'UCG': 'S',
+    'CCG': 'P',
+    'ACG': 'T',
+    'GCG': 'A',
+    'UAU': 'Y',
+    'CAU': 'H',
+    'AAU': 'N',
+    'GAU': 'D',
+    'UAC': 'Y',
+    'CAC': 'H',
+    'AAC': 'N',
+    'GAC': 'D',
+    'UAA': 'Stop',
+    'CAA': 'Q',
+    'AAA': 'K',
+    'GAA': 'E',
+    'UAG': 'Stop',
+    'CAG': 'Q',
+    'AAG': 'K',
+    'GAG': 'E',
+    'UGU': 'C',
+    'CGU': 'R',
+    'AGU': 'S',
+    'GGU': 'G',
+    'UGC': 'C',
+    'CGC': 'R',
+    'AGC': 'S',
+    'GGC': 'G',
+    'UGA': 'Stop',
+    'CGA': 'R',
+    'AGA': 'R',
+    'GGA': 'G',
+    'UGG': 'W',
+    'CGG': 'R',
+    'AGG': 'R',
+    'GGG': 'G'
+}
+
+def get_protein_string(rna_string):
+    protein_string = ''
+    index = 0
+
+    while index < len(rna_string) - 4:
+        codon = rna_string[index:index+3]
+        protein = rna_codon_proteins[codon]
+        protein_string += protein
+        index += 3
+
+    return protein_string
+
+def main():
+    print(read_file(get_protein_string))
+
+if __name__ == '__main__':
+    main()
